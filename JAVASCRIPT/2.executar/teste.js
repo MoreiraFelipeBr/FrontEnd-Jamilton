6dd8021f40703c3e1c9758320281cd428187f362
Felipe Moreira <164668169+MoreiraFelipeBr@users.noreply.github.com>
2025-04-16 11:36:30 -0300

    123

--- JAVASCRIPT/2.executar/teste.js
+++ JAVASCRIPT/2.executar/teste.js
@@ -1,2 +1,20 @@
-var nome = "Helena"
-console.log(nome)
+// //instruções
+/* console.log(
+    "EM uma vasilha, penere a farinha de trigo, o fubá e o fermento para não empelotar"
+  );
+  console.log(
+    "bata as clara em neve"
+  )
+  console.log(
+    "de pro cachorro comer"
+  ) */
+  
+  //bloco de código
+  var vari = 1
+  
+  while (vari < 8) {
+    console.log(`já comeu ${vari} pedaço de pítiça`);
+    vari = vari + 1;
+  }
+
+  
